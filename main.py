0cd77460ca978cdbcc5dd60aa83c4a812055479a
Efrainq07 <33973526+Efrainq07@users.noreply.github.com>
2019-02-15 12:52:51 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,17 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.widget import  Widget
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.lang import  Builder
-from kivy.uix.gridlayout import GridLayout
-from kivy.properties import  ObjectProperty
-
-Builder.load_file("botones.kv")
-Builder.load_file("HCDP.kv")
-
-class Botona(Button):
-    pass
-class Botonb(Button):
-    pass
 
 
-class Container(GridLayout):
-    display = ObjectProperty()
-
-
-    def a(self):
-        self.display.text="a"
-
-    def b(self):
-        self.display.text="b"
+class HCDPBoxLayout(BoxLayout):
+	pass
 
 
 class HCDPApp(App):
+	def build(self):
+		return HCDPBoxLayout()
 
-    def build(self):
-        self.title = 'HCDP'
-        return Container()
 
-ventana = HCDPApp()
-ventana.run()
+
+
+HCDPApp().run()
